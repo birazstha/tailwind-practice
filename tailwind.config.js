f52eb8cadf695774ba1d561eaca53b136caad1b9
biraj1234 <biraj.spell@gmail.com>
2024-08-26 00:39:30 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6347",
+        secondary: "#1E75BC",
+      },
+      fontFamily: {
+        lemon: ["Lemon Milk"],
+        nexa: ["Nexa"],
+      },
+    },
+  },
+  plugins: [],
+};
